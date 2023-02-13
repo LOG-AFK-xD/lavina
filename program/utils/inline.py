0d95262f395167a72d88111098368454aeb74a09
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2023-02-13 12:01:46 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -11,11 +11,21 @@ from pyrogram.types import (
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data=f'cbmenu | {user_id}'),
-      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data=f'cls'),
+      InlineKeyboardButton(text="▢", callback_data=f'cbstop | {user_id}'),
+      InlineKeyboardButton(text="II", callback_data=f'cbpause | {user_id}'),
+      InlineKeyboardButton(text="▷", callback_data=f'cbresume | {user_id}'),
+      InlineKeyboardButton(text="‣‣I", callback_data=f'cbunmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="• Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Team_Bot_Support"),
+      InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs •", url=f"https://t.me/team_bot_update"),
+    ],
+    [
+      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="Cls"),
     ],
   ]
   return buttons
+
 
 
 def menu_markup(user_id):
@@ -31,7 +41,7 @@ def menu_markup(user_id):
       InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs •", url=f"https://t.me/team_bot_update"),
     ],
     [
-      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="set_close"),
+      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="Cls"),
     ],
   ]
   return buttons
